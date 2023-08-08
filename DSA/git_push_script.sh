53c5f300fafd7dd4ac88705870ec7535cd3d614c
SubratYeeshu <shubrat.parth@gmail.com>
2023-08-08 12:25:16 +0530

    Added / Updated Files

---
+++ DSA/git_push_script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Run the necessary Git commands
+git add .
+git commit -m "Added / Updated Files"
+git push
+
+# Display a message after successful execution
+echo "Git commands executed successfully!"
+
